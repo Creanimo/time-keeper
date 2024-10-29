4bc44ca79059152dd26340e3a41717bc98689037
Ferdinand Engländer <ferdinand@creanimo.net>
2024-10-29 13:21:03 +0100

    Squashed 'view/ui-components/strawberry-vanilla-js/' content from commit 3ea2862
    git-subtree-dir: view/ui-components/strawberry-vanilla-js
    git-subtree-split: 3ea28621fe96720d46a3a2333a00a2abe33ce75f

---
+++ ui-component.js
@@ -0,0 +1,73 @@
+import Mustache from "../../node_modules/mustache/mustache.mjs";
+import { htmlStringToElement } from "../tools/htmlStringToElement.js";
+
+/**
+ * Base class for UI components.
+ */
+class UiComponent {
+    /**
+     * Creates an instance of UiComponent.
+     * @param {string} id - The unique identifier for the component.
+     * @param {string} label - The label for the component.
+     */
+    constructor(id, label, name = "ui-component") {
+        this.id = id;
+        this.label = label;
+        this.name = name
+    }
+
+    /**
+     * Gets the properties for rendering the component.
+     * @returns {Object} The properties for rendering.
+     */
+    getRenderProperties() {
+        return {
+            id: this.id,
+            label: this.label,
+            name: this.name,
+        };
+    }
+
+    /**
+     * Sets the template path for the component.
+     * @param {string} path - The path to the template file.
+     */
+    setTemplatePath(path) {
+        this.templatePath = path;
+    }
+
+    /**
+     * Renders the component using the specified template.
+     * @returns {Promise<Node>} The rendered HTML string.
+     */
+    async render(targetNode) {
+        const renderedHtml = await this.renderHTML();
+        targetNode.appendChild(renderedHtml);
+    }
+
+    async renderHTML() {
+        const template = await this.#loadTemplate(this.templatePath);
+        const renderProps = await this.getRenderProperties();
+        const htmlStr = await Mustache.render(template, renderProps); 
+        const renderedHTML = htmlStringToElement(htmlStr);
+        
+        return renderedHTML;
+    }
+
+    /**
+     * Loads the template from the specified path.
+     * @param {string} templatePath - The path to the template file.
+     * @returns {Promise<string>} The template content.
+     * @throws {Error} If the template cannot be loaded.
+     * @private
+     */
+    async #loadTemplate(templatePath) {
+        const response = await fetch(templatePath);
+        if (!response.ok) {
+            throw new Error(`Failed to load template: ${templatePath}`);
+        }
+        return await response.text();
+    }
+}
+
+export { UiComponent };
